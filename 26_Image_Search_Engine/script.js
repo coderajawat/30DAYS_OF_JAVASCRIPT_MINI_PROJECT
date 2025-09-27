3ddf72b816aac096ee1556c92b91df3573568cbe
coderajawat <khushirajawat755@gmail.com>
2025-09-27 23:29:00 +0530

    image search engine

--- 26_Image_Search_Engine/script.js
+++ 26_Image_Search_Engine/script.js
@@ -10,11 +10,15 @@ let page = 1;
 
 async function searchImages() {
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
 
+    if(page === 1){
+        searchResult.innerHTML = "";
+    }
+    
     //console.log(data);
     const results = data.results;
 
@@ -28,10 +32,16 @@ async function searchImages() {
         imageLink.appendChild(image);
         searchResult.appendChild(imageLink);
     })
+    showMoreBtn.style.display = "block";
 }
 
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     page = 1;
+    searchImages();
+})
+
+showMoreBtn.addEventListener("click", () => {
+    page++;
     searchImages();
 })
